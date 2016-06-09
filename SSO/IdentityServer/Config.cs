00fac8ee4094f5e6962f12b1bdd5303475c99028
Leandro Ribeiro <bitbucket@leandroribeiro.com>
2016-06-09 00:24:09 -0300

    Done Sample 1

---
+++ SSO/IdentityServer/Config.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services.InMemory;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static List<InMemoryUser> GetUsers()
+        {
+            return new List<InMemoryUser>
+            {
+                new InMemoryUser
+                {
+                    Subject = "admin", // this is a unique identifier
+                    Username = "admin",
+                    Password = "pass"
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            var clients = new List<Client>();
+            clients.Add(new Client
+            {
+                ClientName = "TestClient",
+                ClientId = "test",
+                Flow = Flows.Implicit,
+                ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                RedirectUris = new List<string>
+                {
+                    "Http://example.local/" //using a fake url for now
+                }
+
+            });
+            return clients;
+        }
+
+        public static IEnumerable<Scope> GetScopes()
+        {
+            var scopes = new List<Scope>();
+            scopes.Add(new Scope
+            {
+                DisplayName = "Api Access",
+                Name = "api",
+                Type = ScopeType.Resource
+            });
+            return scopes;
+        }
+
+
+    }
+}
---
+++ SSO/IdentityServer/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Http;
+using IdentityServer3.AccessTokenValidation;
+using IdentityServer3.Core.Configuration;
+using Owin;
+
+namespace IdentityServer
+{
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
+            {
+                Authority = "http://localhost:51761/",//Your url here
+                RequiredScopes = new[] { "api" }
+            });
+
+
+            app.UseWebApi(config);
+
+            var factory = new IdentityServerServiceFactory()
+                .UseInMemoryUsers(Config.GetUsers())
+                .UseInMemoryClients(Config.GetClients())
+                .UseInMemoryScopes(Config.GetScopes());
+
+            app.UseIdentityServer(new IdentityServerOptions
+            {
+                IssuerUri = "urn:identity",
+                Factory = factory,
+                RequireSsl = false, //DO NOT DO THIS IN PRODUCTION
+
+                LoggingOptions = new LoggingOptions
+                {
+                    EnableWebApiDiagnostics = true,
+                    WebApiDiagnosticsIsVerbose = true
+                },
+
+
+                SigningCertificate = LoadCertificate()
+            });
+
+            //app.UseNLog();
+
+        }
+
+        X509Certificate2 LoadCertificate()
+        {
+            return new X509Certificate2(
+                string.Format(@"{0}\bin\idsrv3test.pfx", AppDomain.CurrentDomain.BaseDirectory), "idsrv3test");
+        }
+
+    }
+}
